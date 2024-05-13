8438563e1d53367ae0dabcc1847baea43bcf58f1
Guillermo Valdes <guillermo@movimientolibre.com>
2024-05-13 12:19:34 -0600

    Detalle del exhorto

--- carina/blueprints/exh_exhortos_partes/views.py
+++ carina/blueprints/exh_exhortos_partes/views.py
@@ -39,8 +39,8 @@ def datatable_json():
         consulta = consulta.filter_by(estatus=request.form["estatus"])
     else:
         consulta = consulta.filter_by(estatus="A")
-    # if "columna_id" in request.form:
-    #     consulta = consulta.filter_by(columna_id=request.form["columna_id"])
+    if "exh_exhorto_id" in request.form:
+        consulta = consulta.filter_by(exh_exhorto_id=request.form["exh_exhorto_id"])
     # if "columna_clave" in request.form:
     #     try:
     #         columna_clave = safe_clave(request.form["columna_clave"])
