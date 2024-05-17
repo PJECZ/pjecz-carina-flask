a3f0df6f527e221d09607cb50a4066591b7cb8b2
Guillermo Valdes <guillermo@movimientolibre.com>
2024-05-17 16:28:19 -0600

    Correccions en externos

--- carina/blueprints/exh_externos/forms.py
+++ carina/blueprints/exh_externos/forms.py
@@ -4,7 +4,9 @@ Exh Externos, formularios
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
+
+from lib.safe_string import URL_REGEXP
 
 
 class ExhExternoForm(FlaskForm):
@@ -13,15 +15,15 @@ class ExhExternoForm(FlaskForm):
     clave = StringField("Clave", validators=[DataRequired(), Length(max=16)])
     descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
     api_key = StringField("API Key", validators=[Optional(), Length(max=128)])
-    endpoint_consultar_materias = StringField("Materias", validators=[Optional(), Length(max=256)])
-    endpoint_recibir_exhorto = StringField("Recibir Exhorto", validators=[Optional(), Length(max=256)])
-    endpoint_recibir_exhorto_archivo = StringField("Recibir Exhorto Archivo", validators=[Optional(), Length(max=256)])
-    endpoint_consultar_exhorto = StringField("Consultar Exhorto", validators=[Optional(), Length(max=256)])
-    endpoint_recibir_respuesta_exhorto = StringField("Recibir Respuesta Exhorto", validators=[Optional(), Length(max=256)])
+    endpoint_consultar_materias = StringField("Materias", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_recibir_exhorto = StringField("Recibir Exhorto", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_recibir_exhorto_archivo = StringField("Recibir Exhorto Archivo", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_consultar_exhorto = StringField("Consultar Exhorto", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_recibir_respuesta_exhorto = StringField("Recibir Respuesta Exhorto", validators=[Optional(), Regexp(URL_REGEXP)])
     endpoint_recibir_respuesta_exhorto_archivo = StringField(
-        "Recibir Respuesta Exhorto Archivo", validators=[Optional(), Length(max=256)]
+        "Recibir Respuesta Exhorto Archivo", validators=[Optional(), Regexp(URL_REGEXP)]
     )
-    endpoint_actualizar_exhorto = StringField("Actualizar Exhorto", validators=[Optional(), Length(max=256)])
-    endpoint_recibir_promocion = StringField("Recibir Promoción", validators=[Optional(), Length(max=256)])
-    endpoint_recibir_promocion_archivo = StringField("Recibir Promoción Archivo", validators=[Optional(), Length(max=256)])
+    endpoint_actualizar_exhorto = StringField("Actualizar Exhorto", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_recibir_promocion = StringField("Recibir Promoción", validators=[Optional(), Regexp(URL_REGEXP)])
+    endpoint_recibir_promocion_archivo = StringField("Recibir Promoción Archivo", validators=[Optional(), Regexp(URL_REGEXP)])
     guardar = SubmitField("Guardar")
