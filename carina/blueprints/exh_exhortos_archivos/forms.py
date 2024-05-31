b85bf2cde126e27b1df07b9b91c18ad925e74843
Guillermo Valdes <guillermo@movimientolibre.com>
2024-05-31 14:05:42 -0600

    Subir archivo

--- carina/blueprints/exh_exhortos_archivos/forms.py
+++ carina/blueprints/exh_exhortos_archivos/forms.py
@@ -3,8 +3,9 @@ Exh Exhorto Archivo, formularios
 """
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length
 
 from carina.blueprints.exh_exhortos_archivos.models import ExhExhortoArchivo
 
@@ -12,23 +13,23 @@ from carina.blueprints.exh_exhortos_archivos.models import ExhExhortoArchivo
 class ExhExhortoArchivoNewForm(FlaskForm):
     """Formulario ExhExhortoArchivoNew"""
 
-    nombre_archivo = StringField("Nombre del Archivo", validators=[DataRequired(), Length(max=256)])
     tipo_documento = SelectField(
         "Tipo", coerce=int, choices=ExhExhortoArchivo.TIPOS_DOCUMENTOS.items(), validators=[DataRequired()]
     )
-    crear = SubmitField("Crear")
+    archivo = FileField("Archivo PDF", validators=[FileRequired()])
+    subir = SubmitField("Subir")
 
 
 class ExhExhortoArchivoEditForm(FlaskForm):
     """Formulario para Editar"""
 
     nombre_archivo = StringField("Nombre del Archivo", validators=[DataRequired(), Length(max=256)])
-    hash_sha1 = StringField("Hash SHA-1")
-    hash_sha256 = StringField("Hash SHA-256")
+    hash_sha1 = StringField("Hash SHA-1")  # Read only
+    hash_sha256 = StringField("Hash SHA-256")  # Read only
     tipo_documento = SelectField(
         "Tipo", coerce=int, choices=ExhExhortoArchivo.TIPOS_DOCUMENTOS.items(), validators=[DataRequired()]
     )
-    url = StringField("Url")
+    url = StringField("URL")
     tamano = StringField("Tamaño")
     fecha_hora_recepcion = StringField("Fecha y hora de recepción")
     guardar = SubmitField("Guardar")
